677e12f924c165fa499ca4c41fb5470572bda92e
tnb w <tnbw.lab@gmail.com>
2023-07-16 21:21:47 +0700

    add refresh token feature

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,6 +1,15 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Get,
+  Req,
+  Post,
+  UseGuards,
+  Headers,
+} from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { AuthDto } from './dto';
+import { JwtRefreshGuard } from './guard';
 
 @Controller('auth')
 export class AuthController {
@@ -14,5 +23,18 @@ export class AuthController {
   @Post('signup')
   signup(@Body() dto: AuthDto) {
     return this.authService.signup(dto);
+  }
+
+  @Post('me')
+  getMe() {
+    return 'my info';
+  }
+
+  @UseGuards(JwtRefreshGuard)
+  @Get('refresh')
+  refreshToken(@Headers('Authorization') header: string, @Req() req: any) {
+    const refreshToken = header.replace('Bearer', '').trim();
+    const userId: number = req.user['sub'];
+    return this.authService.refreshTokens(userId, refreshToken);
   }
 }
